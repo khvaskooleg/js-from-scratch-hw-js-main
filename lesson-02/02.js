126589b5f241c8a42ae312aab35344f60c5973e6
Oleg <khvaskooleg@gmail.com>
2026-05-27 15:07:19 +0300

    Update 02.js

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -9,7 +9,7 @@
 
 let dice1 = Math.floor((Math.random() * 6) + 1)
 let dice2 = Math.floor((Math.random() * 6) + 1)
-let isWinningDouble = (dice1 == dice2 && dice1 > 3)
+let isWinningDouble = (dice1 === dice2 && dice1 > 3)
 
 
 console.log('Первый бросок: ' + dice1)
