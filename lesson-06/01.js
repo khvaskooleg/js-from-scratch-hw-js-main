a840132abef52c59853077b39a9823ca544f16b8
khvaskooleg <khvaskooleg@gmail.com>
2026-06-27 14:50:36 +0300

    lesson 6 01

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -23,4 +23,7 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const filter = filter(array, (element, index) => {
+  return element === true
+});
+
