62dfbfa1dcdfa51b1349b61cd765c177c51a8005
Oleg <khvaskooleg@gmail.com>
2026-08-11 23:54:03 +0300

    lesson-8-03

--- lesson-08/03.js
+++ lesson-08/03.js
@@ -11,7 +11,7 @@
 
 function truncate(str, maxLength) {
   if (str.length > maxLength) {
-    return str.slice(0, maxLength)
+    return str.slice(0, maxLength) + "..."
   } else {
     return str
   }
